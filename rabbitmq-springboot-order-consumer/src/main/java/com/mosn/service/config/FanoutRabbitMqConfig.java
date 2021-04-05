4c8ba7ff32838ed24bb7a88058080b4b6922d74f
mosheng <929262660@qq.com>
2021-04-05 15:46:10 +0800

    rabbitmq

--- rabbitmq-springboot-order-consumer/src/main/java/com/mosn/service/config/FanoutRabbitMqConfig.java
+++ rabbitmq-springboot-order-consumer/src/main/java/com/mosn/service/config/FanoutRabbitMqConfig.java
@@ -1,15 +1,17 @@
-package com.mosn.config;
+package com.mosn.service.config;
 
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+//这些声明放在消费者或者生产者都可以，但是放在消费者这边会更好，
+// 因为生产者启动时，即使交换机或者队列还没声明，启动也不会报错，
+// 而消费者@RabbitListener(queues = {"duanxin.fanout.queue"})指定的队列还不存在，就会报错
+// 因为消费者直接和mq打交道
 @Configuration
-public class RabbitMqConfig {
+public class FanoutRabbitMqConfig {
     //声明fanout模式的交换机
     @Bean
     public FanoutExchange fanoutExchange() {
